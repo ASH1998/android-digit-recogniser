77e87640d757978f1631d5c29853b15411f451e0
Ashutosh <mishra06ashutosh@gmail.com>
2018-07-31 08:21:58 +0530

    add pkl file and nb

--- svc.py
+++ svc.py
@@ -1,21 +1,51 @@
+import cv2
 from sklearn.externals import joblib
-from sklearn import datasets
 from skimage.feature import hog
-from sklearn.svm import LinearSVC
 import numpy as np
 
-dataset_download = datasets.fetch_mldata('MNIST ORIGINAL')
+# Load the classifier
+clf = joblib.load("digits_cls.pkl")
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame2=frame.copy()
+    # Convert to grayscale and apply Gaussian filtering
+    im_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+    ret, im_th = cv2.threshold(im_gray.copy(), 90, 255, cv2.THRESH_BINARY_INV)
+    # Find contours in the binary image 'im_th'
+    _, contours0, hierarchy  = cv2.findContours(im_th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Rectangular bounding box around each number/contour
+    rects = [cv2.boundingRect(ctr) for ctr in contours0]
+    # Draw the bounding box around the numbers
+    for rect in rects:
+        cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
+        # Make the rectangular region around the digit
+        leng = int(rect[3] * 1.6)
+        pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+        pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+        roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+
+    # Resize the image
+    if roi.any():
+        roi = cv2.resize(roi, (28, 28), frame, interpolation=cv2.INTER_AREA)
+        roi = cv2.dilate(roi, (3, 3))
+        roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+        nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+        cv2.putText(im_th, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
 
-features = np.array(dataset.data, 'int16') 
-labels = np.array(dataset.target, 'int')
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    #cv2.imshow('Threshold', im_th)
 
-svc = neW_smv(kernel='rbf',
-	max_pool=122,
-	bach_size=(22,22),
-	epochs=1e-08,
-	)
+    # Press 'q' to exit the video stream
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-def main_driver():
-	if __name__ == '__main()__':
-		run.app()
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
